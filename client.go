d1b40bb24e1c14a247784b8aff07277b39609710
Clint Olson <coandco@gmail.com>
2024-10-12 07:54:01 -0700

    Initial implementation

---
+++ client.go
@@ -0,0 +1,33 @@
+package dreamhost
+
+import (
+	"github.com/adamantal/go-dreamhost/api"
+	"github.com/libdns/libdns"
+)
+
+func (p *Provider) init() error {
+	client, err := api.NewClient(p.APIKey, nil)
+	if err != nil {
+		return err
+	}
+	p.client = *client
+	return nil
+}
+
+func recordFromApiDnsRecord(apiDnsRecord api.DNSRecord) libdns.Record {
+	var rec libdns.Record
+	rec.Type = string(apiDnsRecord.Type)
+	rec.Value = apiDnsRecord.Value
+	// We need to get the name minus the zone to match what libdns expects
+	rec.Name = libdns.RelativeName(apiDnsRecord.Record, apiDnsRecord.Zone)
+	return rec
+}
+
+func apiDnsRecordInputFromRecord(record libdns.Record, zone string) api.DNSRecordInput {
+	var recordInput api.DNSRecordInput
+	recordInput.Type = api.RecordType(record.Type)
+	recordInput.Value = record.Value
+	// Dreamhost expects the record name to be absolute
+	recordInput.Record = libdns.AbsoluteName(record.Name, zone)
+	return recordInput
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/libdns/dreamhost
+
+go 1.18
+
+require (
+	github.com/adamantal/go-dreamhost v0.1.1
+	github.com/libdns/libdns v0.2.2
+)
+
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
